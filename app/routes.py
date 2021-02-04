dd461491bf80b545bd15020ca9c1d7a6f040cab2
faulty-heart <sherry19967@gmail.com>
2021-02-03 19:36:44 -0500

    Use render_template to load index page

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,9 @@
+from flask import render_template
 from app import flask_app
 
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
     user = {'username': 'User1'}
-    return '''
-    <html>
-        <head>
-            <title>Homepage - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello ''' + user['username'] + '''!</h1>
-        </body
-    </html>'''
+    return render_template('index.html', title='Homepage', user=user)
+    
