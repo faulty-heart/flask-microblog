0ddeae42853fc1560569df4e7c026e5a70fb369a
faulty-heart <sherry19967@gmail.com>
2021-03-01 21:08:37 -0500

    Receiving login credentials

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import flask_app
 from app.forms import LoginForm
 
@@ -18,7 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Homepage', user=user, posts=posts)
 
-@flask_app.route('/login')
+@flask_app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
