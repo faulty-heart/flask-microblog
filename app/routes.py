b98ac51696221264ec78780d8694de77a46d2cae
faulty-heart <sherry19967@gmail.com>
2021-03-01 19:16:11 -0500

    Add login view function in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import flask_app
+from app.forms import LoginForm
 
 @flask_app.route('/')
 @flask_app.route('/index')
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Homepage', user=user, posts=posts)
+
+@flask_app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
