a5e0616673deba41bb16c1bde6ed6076e7287e9e
faulty-heart <sherry19967@gmail.com>
2021-03-03 00:57:55 -0500

    Logout view function

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import flask_app
 from app.forms import LoginForm
 from flask_login import current_user, login_user
 from app.models import User
+from flask_login import logout_user
 
 @flask_app.route('/')
 @flask_app.route('/index')
@@ -28,7 +29,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or pasword!")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@flask_app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
